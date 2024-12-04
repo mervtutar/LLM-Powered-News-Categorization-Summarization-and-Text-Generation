bdd5785262937485c5d0bb818422b96d4e200d2a
Merve Tutar <mervetutar78@gmail.com>
2024-12-04 22:19:01 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
-
+###################################################################
+# Patika.dev & NewMind AI Bootcamp Bitirme Projesi
+###################################################################
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
